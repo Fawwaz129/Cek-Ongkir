3c721866fe473da214ec223473797d934408dd8d
Fawwaz129 <wazwazzsrhrr@gmail.com>
2023-04-11 13:17:23 +0700

    Install select2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CitySeeder::class);
         $this->call(CourierSeeder::class);
         $this->call(ProvinceSeeder::class);
+        
     }
 }
